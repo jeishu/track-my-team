edffbed340acb5d6d059b6b38f32043c36509afb
jeishu <jeremy.zhu@utexas.edu>
2021-01-10 17:54:13 -0600

    View different tables in the inquirer.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// ==== Dependencies ====
+// ================ Dependencies ================
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
@@ -17,25 +17,107 @@ connection.connect(function(err) {
     startPrompt();
 });
 
-function startPrompt() {
+// ================ Starting Prompt ================
+const startPrompt = () => {
     inquirer.prompt([
         {
             type: "list",
             message: "Main Menu",
             name: "choice",
             choices: [
-                "View all employee",
-                "View all employee by Departments",
-                "View all employees by Roles",
+                "View all employees by name",
+                "View all employees by roles",
+                "View all employees by departments",
+                // "View all employees by manager",
                 "Add an employee",
                 "Add a role",
                 "Add a department",
-                "Update employee roles"
+                "Update employee roles",
+                // "Update employee manager",
+                // "Delete an employee",
+                // "Delete a role",
+                // "Delete a department"
             ]
         }
-        
     ])
     .then((answer) => {
-        
+        switch (answer.choice) {
+            case "View all employees by name":
+                viewEmployees();
+                break;
+            case "View all employees by roles":
+                viewRoles();
+                break;
+            case "View all employees by departments":
+                viewDepartments();
+                break;
+            // case "View all employees by manager":
+            //     viewManager();
+            //     break;    
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "Add a department":
+                addDepartment();
+                break;
+            // case "Update employee roles":
+            //     updateEmployeeRoles();
+            //     break;
+            // case "Update employee manager":
+            //     updateManager();
+            //     break;
+            // case "Delete an employee":
+            //     deleteEmployee();
+            //     break; 
+            // case "Delete a role":
+            //     deleteRole();
+            //     break;
+            // case "Delete a department":
+            //     deleteDepartment();
+            //     break;
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+}
+
+// ================ Functions ================
+const viewEmployees = () => {
+    connection.query("SELECT * FROM employee;",
+    function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        startPrompt();
     })
 }
+const viewRoles = () => {
+    connection.query("SELECT * FROM roles;",
+    function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        startPrompt();
+    })
+}
+const viewDepartments = () => {
+    connection.query("SELECT * FROM department;",
+    function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        startPrompt();
+    })
+}
+// const viewManager = () => {}   
+
+const addEmployee = () => {}
+const addDepartment = () => {}
+
+// const updateEmployeeRoles = () => {}
+// const updateManager = () => {}
+
+// const deleteEmployee = () => {}
+// const deleteRole = () => {}
+// const deleteDepartment = () => {}
--- db.sql
+++ db.sql
@@ -2,19 +2,22 @@ DROP DATABASE IF EXISTS employee_db;
 
 CREATE DATABASE employee_db;
 
+USE employee_db;
+
 CREATE TABLE department (
     id INT AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
+    name_dept VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    SALARY DECIMAL NOT NULL,
+    salary DECIMAL(12, 2) NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
-)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT,
@@ -22,5 +25,40 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id)
-)
+    is_manager BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (id),
+	FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+INSERT INTO department (name_dept)
+VALUES 
+("IT"), -- 1
+("Sales"), -- 2
+("HR"), -- 3
+("Marketing"); -- 4
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+("IT Manager", 90000.00, 1),
+("Sales Manager", 80000.00, 2),
+("HR Manager", 70000.00, 3),
+("Marketing Manager", 70000.00, 4),
+("Software Engineer", 90000.00, 1),
+("Sales Consultant", 45000.00, 2),
+("HR Specialist", 60000.00, 3),
+("Market Research Analyst", 65000.00, 4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id, is_manager)
+VALUES 
+("Jay", "Zhang", 1, NULL , TRUE),
+("Jae", "Zhong", 2, NULL , TRUE),
+("Jai", "Zheng", 3, NULL , TRUE),
+("Jei", "Zhung", 4, NULL , TRUE),
+("Kay","Lei", 5, 1, FALSE),
+("Key","Lai", 6, 2, FALSE),
+("Kai","Lay", 7, 3, FALSE),
+("Kei","Ley", 8, 4, FALSE);
+
+SELECT * FROM employee WHERE is_manager = TRUE;
